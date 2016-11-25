cef0b760ae3769102a6f8c9baf63b9b8afa40ae7
Sasha Goldshtein <goldshtn@gmail.com>
2016-11-25 16:25:12 +0200

    Add skeleton for lab 1 - dot product

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace simd_workshop
     {
         static void Main(string[] args)
         {
+            BenchmarkRunner.Run<SimpleVectors>();
         }
     }
 }
---
+++ SimpleVectors.cs
@@ -0,0 +1,59 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simd_workshop
+{
+    public class SimpleVectors
+    {
+        private readonly float[] a = new float[4096];
+        private readonly float[] b = new float[4096];
+        private readonly float[] c = new float[4096];
+
+        [Benchmark]
+        public void AddVectors()
+        {
+            int length = a.Length;
+            for (int i = 0; i < length; ++i)
+                c[i] = a[i] + b[i];
+        }
+
+        [Benchmark]
+        public void AddVectorsSimd()
+        {
+            int vectorLength = Vector<float>.Count;
+            int remainder = a.Length % vectorLength, length = a.Length - remainder;
+            for (int i = 0; i < length; i += vectorLength)
+            {
+                Vector<float> va = new Vector<float>(a, i);
+                Vector<float> vb = new Vector<float>(b, i);
+                Vector<float> vc = va + vb;
+                vc.CopyTo(c, i);
+            }
+            for (int i = 0; i < remainder; ++i)
+            {
+                c[length + i] = a[length + i] + b[length + i];
+            }
+        }
+
+        [Benchmark]
+        public float DotProduct()
+        {
+            float sum = 0.0f;
+            for (int i = 0; i < a.Length; ++i)
+                sum += a[i] * b[i];
+            return sum;
+        }
+
+        [Benchmark]
+        public float DotProductSimd()
+        {
+            // TODO Implement this
+            return 0.0f;
+        }
+    }
+}
