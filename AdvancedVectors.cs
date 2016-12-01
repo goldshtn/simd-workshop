4904cb5d7e4c2e6feb5c1b5d7f28990b41ec121e
Sasha Goldshtein <goldshtn@gmail.com>
2016-12-01 18:16:45 +0200

    Add set intersect SIMD implementation and benchmark

--- AdvancedVectors.cs
+++ AdvancedVectors.cs
@@ -15,6 +15,9 @@ namespace simd_workshop
         private string _needle;
         private byte[] _haystackBytes;
         private byte[] _needleBytes;
+        private short[] _set1;
+        private short[] _set2;
+        private short[] _output;
 
         [Setup]
         public void Setup()
@@ -25,6 +28,17 @@ namespace simd_workshop
             _haystack = Encoding.ASCII.GetString(_haystackBytes);
             _needle = "Hello, earthling.";
             _needleBytes = Encoding.ASCII.GetBytes(_needle);
+
+            _set1 = new short[4096];
+            _set2 = new short[4096];
+            _output = new short[4096]; // At most, all elements are shared
+            for (int i = 0; i < _set1.Length; ++i)
+            {
+                _set1[i] = (short)random.Next(short.MinValue, short.MaxValue);
+                _set2[i] = (short)random.Next(short.MinValue, short.MaxValue);
+            }
+            Array.Sort(_set1);
+            Array.Sort(_set2);
         }
 
         [Benchmark]
@@ -42,5 +56,38 @@ namespace simd_workshop
         [DllImport(@"C:\dev\simd-workshop\x64\Release\native-helpers.dll",
                    CallingConvention = CallingConvention.StdCall)]
         private static extern int str_str(byte[] haystack, byte[] needle, int hsSize, int needleSize);
+
+        [Benchmark]
+        public void SetIntersect()
+        {
+            int i = 0, j = 0;
+            int outCounter = 0;
+            while (i < _set1.Length && j < _set2.Length)
+            {
+                if (_set1[i] < _set2[j])
+                {
+                    i++;
+                }
+                else if (_set2[j] < _set1[i])
+                {
+                    j++;
+                }
+                else
+                {
+                    _output[outCounter++] = _set1[i];
+                    i++; j++;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void SetIntersectSimd()
+        {
+            set_intersect(_set1, _set2, _set1.Length, _output);
+        }
+
+        [DllImport(@"C:\dev\simd-workshop\x64\Release\native-helpers.dll",
+                   CallingConvention = CallingConvention.StdCall)]
+        private static extern uint set_intersect(short[] A, short[] B, int size, short[] C);
     }
 }
--- native-helpers/setintersect.cpp
+++ native-helpers/setintersect.cpp
@@ -1,10 +1,54 @@
 #include <nmmintrin.h>
+#include <memory.h>
 
-extern "C" __declspec(dllexport) void set_intersect(short* a, short* b, int size, short* c)
+// Adapted from https://highlyscalable.wordpress.com/2012/06/05/fast-intersection-sorted-lists-sse/
+
+static __m128i shuffle_mask[16];
+
+int getBit(int value, int position) {
+    return ( ( value & (1 << position) ) >> position);
+}
+
+void prepare_shuffling_dictionary() {
+    for(int i = 0; i < 16; i++) {
+        int counter = 0;
+        char permutation[16];
+        memset(permutation, 0xFF, sizeof(permutation));
+        for(char b = 0; b < 4; b++) {
+            if(getBit(i, b)) {
+                permutation[counter++] = 4*b;
+                permutation[counter++] = 4*b + 1;
+                permutation[counter++] = 4*b + 2;
+                permutation[counter++] = 4*b + 3;
+            }
+        }
+        __m128i mask = _mm_loadu_si128((const __m128i*)permutation);
+        shuffle_mask[i] = mask;
+    }
+}
+ 
+extern "C" __declspec(dllexport) size_t set_intersect(short* A, short* B, int size, short* C)
 {
-	// __m128i va, vb;
-	// __m128i res = _mm_cmpestrm(va, 8, vb, 8,
-	//	_SIDD_UWORD_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_BIT_MASK);
-	// Shuffle common elements, write out common elements, keep going:
-	//	https://highlyscalable.wordpress.com/2012/06/05/fast-intersection-sorted-lists-sse/
+    size_t count = 0;
+    size_t i_a = 0, i_b = 0;
+ 
+    while(i_a < size && i_b < size) {
+        __m128i v_a = _mm_loadu_si128((__m128i*)&A[i_a]);
+        __m128i v_b = _mm_loadu_si128((__m128i*)&B[i_b]);
+ 
+        __m128i res_v = _mm_cmpestrm(v_b, 8, v_a, 8,
+                _SIDD_UWORD_OPS|_SIDD_CMP_EQUAL_ANY|_SIDD_BIT_MASK);
+        int r = _mm_extract_epi32(res_v, 0);
+        __m128i p = _mm_shuffle_epi8(v_a, shuffle_mask[r]); // TODO Verify shuffle mask
+        _mm_storeu_si128((__m128i*)&C[count], p);
+        count += _mm_popcnt_u32(r);
+ 
+        short a_max = _mm_extract_epi16(v_a, 7);
+        short b_max = _mm_extract_epi16(v_b, 7);
+        i_a += (a_max <= b_max) * 4;
+        i_b += (a_max >= b_max) * 4;
+    }
+ 
+	// TODO Take care of the tail 
+    return count;
 }
